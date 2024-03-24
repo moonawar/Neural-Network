55f5c08ed9be3ca06adec3b6f81601d96e2b3e7f
dikyrest <13520017@std.stei.itb.ac.id>
2024-03-24 15:58:53 +0700

    test ffnn class

--- Bagian-A/src/main.py
+++ Bagian-A/src/main.py
@@ -1,19 +1,23 @@
 import json
 import numpy as np
+from ffnn import FFNN
 from layer import Layer
 
 model = open(f'Bagian-A/test/relu.json', 'r')
 model = json.load(model)
 
-neuron = model['case']['model']['layers'][0]['number_of_neurons']
-activation_function = model['case']['model']['layers'][0]['activation_function']
-weights = np.array(model['case']['weights'][0][1:])
-bias = np.array(model['case']['weights'][0][0])
+layers = model['case']['model']['layers']
+weights = model['case']['weights']
 
-layer = Layer(neuron, activation_function, weights, bias)
-input = model['case']['input']
+ffnn = FFNN()
+for i in range (len(layers)):
+    layer = layers[i]
+    weight = weights[i]
+    ffnn.add_layer(Layer(layer["number_of_neurons"], layer["activation_function"], np.array(weight[1:]), np.array(weight[0])))
 
-output = layer.forward(input)
+input = model["case"]["input"]
+
+output = ffnn.forward(input)
 expected_output = model['expect']['output']
 
 print(f'output: {output}')
