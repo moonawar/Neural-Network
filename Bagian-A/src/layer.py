2125f96ca69f3f17419ae0f8934412b5e7c1dd82
dikyrest <13520017@std.stei.itb.ac.id>
2024-03-24 15:10:56 +0700

    add layer class

---
+++ Bagian-A/src/layer.py
@@ -0,0 +1,24 @@
+import numpy as np
+from activation import activation
+
+class Layer:
+    def __init__(self, neuron: int, activation_function: str, weights: np.array, bias: list):
+        self.neuron = neuron
+        self.weights = weights
+        self.bias = bias
+        if activation_function not in activation:
+            raise Exception('Invalid activation function')
+        else:
+            self.activation_function = activation_function
+            if activation_function == 'sigmoid':
+                self.function = lambda net: 1 / (1 + np.exp(-net))
+            elif activation_function == 'relu':
+                self.function = lambda net: np.maximum(0, net)
+            elif activation_function == 'linear':
+                self.function = lambda net: net
+            elif activation_function == 'softmax':
+                self.function = lambda net: np.exp(net) / np.sum(np.exp(net))
+
+    def forward(self, input: np.array):
+        self.output = self.function(np.dot(input, self.weights) + self.bias)
+        return self.output
